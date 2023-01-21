e97913de30bdcef9c9917f9e544e246364d5e944
Sandoo <san02@kakao.com>
2023-01-22 00:24:04 +0900

    파일 분리 및 import

--- functional_js/js/func.js
+++ functional_js/js/func.js
@@ -1,18 +1,16 @@
 const _filter = (list, predi) => {
   let new_list = [];
-  for (let i = 0; i < list.length; i++) {
-    if (predi(list[i])) {
-      new_list.push(list[i]);
-    }
-  }
+  _each(list, function (val) {
+    if (predi(val)) new_list.push(val);
+  });
   return new_list;
 };
 
 function _map(list, mapper) {
   let new_list = [];
-  for (let i = 0; i < list.length; i++) {
-    new_list.push(mapper(list[i]));
-  }
+  _each(list, function (val) {
+    new_list.push(mapper(val));
+  });
   return new_list;
 }
 
