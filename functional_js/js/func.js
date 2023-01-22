0b851f0c15bb04aaf7390c59e727bc7c6ad21123
Sandoo <san02@kakao.com>
2023-01-22 15:11:25 +0900

    _pipe, _go

--- functional_js/js/func.js
+++ functional_js/js/func.js
@@ -1,10 +1,10 @@
-const _filter = (list, predi) => {
+function _filter(list, predi) {
   let new_list = [];
   _each(list, function (val) {
     if (predi(val)) new_list.push(val);
   });
   return new_list;
-};
+}
 
 function _map(list, mapper) {
   let new_list = [];
@@ -41,3 +41,37 @@ function _curryr(fn) {
 let _get = _curryr(function (obj, key) {
   return obj == null ? undefined : obj[key];
 });
+
+var _map = _curryr(_map),
+  _filter = _curryr(_filter);
+function _rest(list, num) {
+  return slice.call(list, num || 1);
+}
+function _reduce(list, iter, memo) {
+  if (arguments.length == 2) {
+    memo = list[0];
+    list = _rest(list);
+  }
+  _each(list, val => {
+    memo = iter(memo, val);
+  });
+  return memo;
+}
+
+function _pipe() {
+  let fns = arguments;
+  return function (arg) {
+    return _reduce(
+      fns,
+      (arg, fn) => {
+        return fn(arg);
+      },
+      arg
+    );
+  };
+}
+
+function _go(arg) {
+  let fns = _rest(arguments);
+  return _pipe.apply(null, fns)(arg);
+}
