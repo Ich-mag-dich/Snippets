b0d4c1b43820c833d55bc2f7b97be7500bf6e48d
asdasd <92120487+Ich-mag-dich@users.noreply.github.com>
2023-01-26 01:30:59 +0900

    test

--- go/test/test.go
+++ go/test/test.go
@@ -2,29 +2,40 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func getPage(link string) string {
+func getH1(link, tag string) {
 	res, err := http.Get(link)
 	if err != nil {
 		log.Fatal(err)
 	}
-	body, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
+	// body, err := io.ReadAll(res.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", body)
-	return string(body)
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res.Body.Close()
+	doc.Find(tag).Each(func(i int, s *goquery.Selection) {
+		fmt.Printf("%d : %s\n", i, s.Text())
+	})
+	// fmt.Printf("%s", body)
 }
 
 func main() {
 	fmt.Println("hello world!")
-	var w1 string
+	var w1, w2 string
+	fmt.Print("Input Url: ")
 	fmt.Scanln(&w1)
-	fmt.Println(getPage(w1))
+	fmt.Print("Input tag: ")
+	fmt.Scanln(&w2)
+	getH1(w1, w2)
 
+	// testdns(w1)
 }
