1494a18ba9e3600028f7519aba9f467b750599c6
Ich-mag-dich <san02@kakao.com>
2023-03-06 03:27:08 +0900

    test

--- go/code_space/main.go
+++ go/code_space/main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello code space")
+	fmt.Printf("hello %s! \n",hello("code space"))
+}
+
+func hello(arg1 string) string {
+  return arg1
 }
