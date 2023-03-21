5b35094d902e7d6afcd5b5e319024ef54f9c557a
Ich-mag-dich <san02@kakao.com>
2023-03-21 22:06:42 +0900

    hello emacs

--- python/test.py
+++ python/test.py
@@ -1,5 +1,9 @@
+def hello_emacs():
+    return "Hello emacs!"
+
 def test(message):
     return len(message)
 if __name__ == "__main__":
     print(f"length: {test(str(input('message 입력: ')))}")
+    print(hello_emacs)
 
