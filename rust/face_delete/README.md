17419fd980ccc06f14b82934bba74bcd195c9565
Ich-mag-dich <san02@kakao.com>
2023-03-23 13:40:21 +0900

    face delete

---
+++ rust/face_delete/README.md
@@ -0,0 +1,4 @@
+# face delete
+
+> use
+> [https://github.com/atomashpolskiy/rustface](https://github.com/atomashpolskiy/rustface)
--- rust/face_delete/src/main.rs
+++ rust/face_delete/src/main.rs
@@ -1,63 +1,109 @@
-use std::env::Args;
-use std::time::{Duration, Instant};
-
 use image::{DynamicImage, GrayImage, Rgb};
 use imageproc::drawing::draw_filled_rect_mut;
 use imageproc::rect::Rect;
+use std::fs::read_dir;
+use std::io::Write;
+use std::time::{Duration, Instant};
+use std::{fs, io};
 
 use rustface::{Detector, FaceInfo, ImageData};
 
-// let mut output_file: &str = "test.png";
-// TODO  인풋폴더, 아웃풋폴더 정해서 인풋폴더에있는 사진들 모두 가리게 반복하는 기능.
 fn main() {
-    let mut line = String::new();
-    println!("Enter your name :");
-    let read_line = std::io::stdin().read_line(&mut line).unwrap();
-    let output_file = format!("{}.png", &line.trim());
-    let output_file = output_file.as_str();
-    println!("output file: {output_file}, filename bytes: {read_line}");
-    let options = match Options::parse(std::env::args()) {
-        Ok(options) => options,
-        Err(message) => {
-            println!("Failed to parse program arguments: {}", message);
-            std::process::exit(1)
+    print!("Input image path: ");
+    io::stdout().flush().unwrap();
+    let mut input_path = String::new();
+    io::stdin().read_line(&mut input_path).expect("input error");
+    let input_path = input_path.as_str().trim();
+    println!(
+        "\n\n------------------------------------------\nimage path is: {}",
+        input_path
+    );
+    let path = read_dir(&input_path).unwrap();
+    // println!("files: {}", &path.count());
+    let mut file_count = 0;
+    let mut files = vec![];
+    let model_path = "./model/seeta_fd_frontal_v1.0.bin";
+    for i in path {
+        match i {
+            Ok(file) => {
+                if file.file_name() != ".DS_Store" {
+                    let file_type_num = file.file_type();
+                    let file_type_num = match file_type_num {
+                        Ok(num) => num,
+                        Err(_) => panic!(),
+                    };
+
+                    println!(
+                        "------------------------------------------\n● {:?}",
+                        file.file_name()
+                    );
+                    if file_type_num.is_dir() {
+                        println!("this is not image");
+                        continue;
+                    }
+                    file_count += 1;
+                    files.push(file.file_name());
+                }
+            }
+
+            Err(_) => println!("error"),
         }
-    };
-
-    let mut detector = match rustface::create_detector(options.model_path()) {
-        Ok(detector) => detector,
-        Err(error) => {
-            println!("Failed to create detector: {}", error.to_string());
-            std::process::exit(1)
-        }
-    };
-
-    detector.set_min_face_size(20);
-    detector.set_score_thresh(0.8); // first: 2.0, 0.95: found 22 faces
-    detector.set_pyramid_scale_factor(0.85); // 0.8 is found 11 faces, 0.99 is found 14 faces but actually 13 faces
-    detector.set_slide_window_step(2, 2);
-    let image: DynamicImage = match image::open(options.image_path()) {
-        Ok(image) => image,
-        Err(message) => {
-            println!("Failed to read image: {}", message);
-            std::process::exit(1)
-        }
-    };
-
-    let mut rgb = image.to_rgb8();
-    let faces = detect_faces(&mut *detector, &image.to_luma8());
-
-    for face in faces {
-        let bbox = face.bbox();
-        let rect = Rect::at(bbox.x(), bbox.y()).of_size(bbox.width(), bbox.height());
-
-        draw_filled_rect_mut(&mut rgb, rect, Rgb([0, 0, 0]));
     }
 
-    match rgb.save(output_file) {
-        Ok(_) => println!("Saved result to {}", output_file),
-        Err(message) => println!("Failed to save result to a file. Reason: {}", message),
+    println!("------------------------------------------\nimage count: {file_count}");
+    let mut count = 0;
+    while count < file_count {
+        let mut detector = match rustface::create_detector(&model_path) {
+            Ok(detector) => detector,
+            Err(error) => {
+                println!("Failed to create detector: {}", error.to_string());
+                std::process::exit(1)
+            }
+        };
+        let file_path = format!("{input_path}{:?}", &files[count]);
+
+        let file_path = file_path.replace("\"", "");
+        println!("\n{}/{file_count}\n{file_path}", &count + 1);
+        detector.set_min_face_size(20);
+        detector.set_max_face_size(400);
+        detector.set_score_thresh(0.9); // first: 2.0, 0.95: found 22 faces
+        detector.set_pyramid_scale_factor(0.9); // 0.8 is found 11 faces, 0.99 is found 14 faces but actually 13 faces
+        detector.set_slide_window_step(4, 4);
+        let image: DynamicImage = match image::open(&file_path) {
+            Ok(image) => image,
+            Err(message) => {
+                println!("Failed to read image: {}", message);
+                std::process::exit(1)
+            }
+        };
+
+        let mut rgb = image.to_rgb8();
+        let faces = detect_faces(&mut *detector, &image.to_luma8());
+
+        for face in faces {
+            let bbox = face.bbox();
+            let rect = Rect::at(bbox.x(), bbox.y()).of_size(bbox.width(), bbox.height());
+
+            draw_filled_rect_mut(&mut rgb, rect, Rgb([255, 255, 255]));
+        }
+        let output_file = format!(
+            "./save/{input_path}save_{}",
+            &files[count].to_str().unwrap()
+        );
+        match rgb.save(&output_file) {
+            Ok(_) => println!("Saved result to {}", output_file),
+            Err(message) => {
+                let create_dir_name = format!("./save/{input_path}");
+                fs::create_dir_all(create_dir_name.as_str()).unwrap();
+                match rgb.save(&output_file) {
+                    Ok(_) => println!("Save result to {}", output_file),
+                    Err(message2) => println!("can't save file, {}, {}", message, message2),
+                }
+            }
+        }
+        count += 1;
     }
+    println!("\n--------------------------------------------\nDone!");
 }
 
 fn detect_faces(detector: &mut dyn Detector, gray: &GrayImage) -> Vec<FaceInfo> {
@@ -75,34 +121,4 @@ fn detect_faces(detector: &mut dyn Detector, gray: &GrayImage) -> Vec<FaceInfo> 
 
 fn get_millis(duration: Duration) -> u64 {
     duration.as_secs() * 1000u64 + u64::from(duration.subsec_nanos() / 1_000_000)
-}
-
-struct Options {
-    image_path: String,
-    model_path: String,
-}
-
-impl Options {
-    fn parse(args: Args) -> Result<Self, String> {
-        let args: Vec<String> = args.into_iter().collect();
-        if args.len() != 3 {
-            return Err(format!("Usage: {} <model-path> <image-path>", args[0]));
-        }
-
-        let model_path = args[1].clone();
-        let image_path = args[2].clone();
-
-        Ok(Options {
-            image_path,
-            model_path,
-        })
-    }
-
-    fn image_path(&self) -> &str {
-        &self.image_path[..]
-    }
-
-    fn model_path(&self) -> &str {
-        &self.model_path[..]
-    }
 }
