ca345d73c6a59baa341fc2d6ec15c2bd20605734
Ich-mag-dich <san02@kakao.com>
2023-04-06 11:18:38 +0900

    commit

--- python/hello.py
+++ python/hello.py
@@ -1,5 +1,9 @@
 def hello_vim():
     return "hello vim!"
 
+def hello_fedora():
+    return "hello fedora"
+
 if __name__ == "__main__":
     print(f"hello world! {hello_vim()}")
+    print(f"hello world! {hello_fedora()}")
--- python/test.py
+++ python/test.py
@@ -3,6 +3,7 @@ def hello_emacs():
 
 def test(message):
     return len(message)
+
 if __name__ == "__main__":
     print(f"length: {test(str(input('message 입력: ')))}")
     print(hello_emacs)
